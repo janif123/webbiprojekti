b8805a505051b0dde50eef2ee5511a9215e10b54
Jani Flygare <flygare.jani12gmail.com>
2017-09-26 19:33:41 +0300

    added toggleClass function

--- Script.js
+++ Script.js
@@ -6,3 +6,8 @@ function showText(text) {
    let elem = document.getElementById("teksti-kappale");
     elem.textContent = text;
 }
+
+function toggleClass(elemId, className){
+  let elem = document.getElementById(elemId);  
+    elem.classList.toggle(className); 
+}
