29b5ba7a803dfb4ae21ba3c8882056ecba625279
Jani Flygare <flygare.jani12gmail.com>
2017-10-03 19:38:32 +0300

    added switch

--- Script.js
+++ Script.js
@@ -11,3 +11,8 @@ function toggleClass(elemId, className){
   let elem = document.getElementById(elemId);  
     elem.classList.toggle(className); 
 }
+
+function switchImage(imgId, path) {
+   let img = document.getElementById(imgId);
+   img.src = path; 
+}
